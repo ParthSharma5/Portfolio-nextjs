f79c685cbb3edadba19f0ee8d900b95a10222ab1
Parth Sharma <sharmap55999@gmail.com>
2025-09-04 14:48:07 +0530

    Add Google SEO verification meta tag

--- myportfolio/src/app/layout.js
+++ myportfolio/src/app/layout.js
@@ -1,3 +1,32 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// export const metadata = {
+//   title: "Parth Sharma's Portfolio",
+//   description: "Frontend Developer",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -14,6 +43,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Parth Sharma's Portfolio",
   description: "Frontend Developer",
+  verification: {
+    google: "J0Hadec8T4khVjIDLK8JJ350BK-6ZBzvVeN5xi26fk4", // ✅ Add this line
+  },
 };
 
 export default function RootLayout({ children }) {
