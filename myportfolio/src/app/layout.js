f5b2f9e1679dddd7eca185b6549eba0ac72d6607
Parth Sharma <sharmap55999@gmail.com>
2025-09-21 01:10:10 +0530

    change in layout file to get favicon

--- myportfolio/src/app/layout.js
+++ myportfolio/src/app/layout.js
@@ -1,32 +1,4 @@
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata = {
-//   title: "Parth Sharma's Portfolio",
-//   description: "Frontend Developer",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//       >
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -39,14 +11,20 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata = {
   title: "Parth Sharma's Portfolio",
   description: "Frontend Developer",
-  verification: {
-    google: "J0Hadec8T4khVjIDLK8JJ350BK-6ZBzvVeN5xi26fk4", // ✅ Add this line
+  icons: {
+    // icon: "/favicon.ico", // favicon
+    // Change here in icon place to get the favicon 
+    icon:"/favicon.svg",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png", // Optional
   },
 };
+
+
+
 
 export default function RootLayout({ children }) {
   return (
