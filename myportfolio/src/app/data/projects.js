5a7841d41a23ed10e18d994a96c9e7407b195522
Parth Sharma <sharmap55999@gmail.com>
2026-02-22 01:27:45 +0530

    Updated the layout for the projects part

--- myportfolio/src/app/data/projects.js
+++ myportfolio/src/app/data/projects.js
@@ -1,78 +1,6 @@
 const projects = [
   {
     id: 1,
-    title: "Books Store",
-    description:
-      "Add your favorite book to our community recommendations by simply entering the book title, giving it a rating out of 5, and uploading or pasting an image of the book cover. You can also include a short note about why this book means so much to you.",
-    link: "",
-    github: "https://github.com/ParthSharma5/Bookstore-Frontend",
-    languagesUsed: ["React Native", "MongoDB", "Express", "Node.js"],
-    status: "Not Live",
-    featured: true,
-    date: "2024",
-    duration: "2 months",
-    team: "Solo",
-  },
-  {
-    id: 2,
-    title: "TinDog Website",
-    description:
-      "TinDog is a playful dating app for dogs helping your pup meet new furry friends nearby. With simple usability, flexible subscription plans, and a money-back guarantee, it's the perfect way for your dog to find their true love.",
-    status: "Live",
-    link: "https://tindog-website-six-navy.vercel.app/",
-    github: "https://github.com/ParthSharma5/tindog-website",
-    languagesUsed: ["JavaScript", "HTML", "CSS"],
-    featured: false,
-    date: "2024",
-    duration: "1 month",
-    team: "Solo",
-  },
-  {
-    id: 3,
-    title: "WhatsApp Clone",
-    description:
-      "This project is a fully functional WhatsApp clone developed using React Native, designed to replicate the core features of the popular messaging app including real-time messaging, user authentication, and chat rooms.",
-    status: "Not Live",
-    link: "",
-    github: "https://github.com/ParthSharma5/Clone",
-    languagesUsed: ["React Native", "Firebase", "JavaScript"],
-    featured: true,
-    date: "2024",
-    duration: "3 months",
-    team: "Solo",
-  },
-  {
-    id: 4,
-    title: "Spotify Clone",
-    description:
-      "A fully functional Spotify-inspired music streaming web application built using HTML, CSS, and JavaScript, focused on delivering an immersive UI experience without using any frameworks. The project includes real-time audio controls, playlist switching, play/pause functionality, and a responsive layout optimized for all devices. Designed to replicate the core feel of Spotify’s interface while demonstrating strong skills in DOM manipulation, event handling, and front-end UI development.",
-
-    status: "Live",
-    link: "https://spotifyclone-h3bw4xsoi-parth-sharmas-projects-2f98a91f.vercel.app/", // Use # for upcoming projects or remove link
-    github: "https://github.com/ParthSharma5/spotify-clone", // Add your GitHub link
-    languagesUsed: ["JavaScript", "HTML", "CSS"],
-    featured: false,
-    date: "2025",
-    duration: "1 months",
-    team: "Solo",
-  },
-
-  {
-    id: 5,
-    title: "Real-time Chat App",
-    description:
-      "A full-featured real-time chat application built using React Native, Supabase, and Clerk for authentication. This project focuses on creating a fast, secure, and scalable mobile chat experience with modern backend services.",
-    status: "Not Live",
-    link: "",
-    github: "https://github.com/ParthSharma5/RealChat",
-    languagesUsed: ["React Native", "TypeScript", "Supabase", "Clerk"],
-    featured: false,
-    date: "2026",
-    duration: "2 months",
-    team: "Solo",
-  },
-  {
-    id: 6,
     title: "SnapSearch",
     description:
       "SnapSearch is a React-based image search application that allows users to search for and view high-quality images from the  API. The app features a clean and responsive design, real-time search functionality, and infinite scrolling for a seamless user experience.",
@@ -86,7 +14,36 @@ const projects = [
     team: "Solo",
   },
   {
-    id: 7,
+    id: 2,
+    title: "Spotify Clone",
+    description:
+      "A fully functional Spotify-inspired music streaming web application built using HTML, CSS, and JavaScript, focused on delivering an immersive UI experience without using any frameworks. The project includes real-time audio controls, playlist switching, play/pause functionality, and a responsive layout optimized for all devices. Designed to replicate the core feel of Spotify’s interface while demonstrating strong skills in DOM manipulation, event handling, and front-end UI development.",
+
+    status: "Live",
+    link: "https://spotifyclone-h3bw4xsoi-parth-sharmas-projects-2f98a91f.vercel.app/", // Use # for upcoming projects or remove link
+    github: "https://github.com/ParthSharma5/spotify-clone", // Add your GitHub link
+    languagesUsed: ["JavaScript", "HTML", "CSS"],
+    featured: false,
+    date: "2025",
+    duration: "1 months",
+    team: "Solo",
+  },
+  {
+    id: 3,
+    title: "TinDog Website",
+    description:
+      "TinDog is a playful dating app for dogs helping your pup meet new furry friends nearby. With simple usability, flexible subscription plans, and a money-back guarantee, it's the perfect way for your dog to find their true love.",
+    status: "Live",
+    link: "https://tindog-website-six-navy.vercel.app/",
+    github: "https://github.com/ParthSharma5/tindog-website",
+    languagesUsed: ["JavaScript", "HTML", "CSS"],
+    featured: false,
+    date: "2024",
+    duration: "1 month",
+    team: "Solo",
+  },
+  {
+    id: 4,
     title: "QuickMd",
     description:
       "A markdwown editor built using React, allowing users to write and preview markdown content in real-time. The app features a split-screen interface with a textarea for input and a live preview pane that renders the formatted markdown, making it an ideal tool for writers, developers, and anyone who needs to create markdown content efficiently.",
@@ -97,6 +54,50 @@ const projects = [
     featured: false,
     date: "2026",
     duration: "1 month",
+    team: "Solo",
+  },
+  {
+    id: 5,
+    title: "Books Store",
+    description:
+      "Add your favorite book to our community recommendations by simply entering the book title, giving it a rating out of 5, and uploading or pasting an image of the book cover. You can also include a short note about why this book means so much to you.",
+    link: "",
+    github: "https://github.com/ParthSharma5/Bookstore-Frontend",
+    languagesUsed: ["React Native", "MongoDB", "Express", "Node.js"],
+    status: "Not Live",
+    featured: true,
+    date: "2024",
+    duration: "2 months",
+    team: "Solo",
+  },
+
+  {
+    id: 6,
+    title: "WhatsApp Clone",
+    description:
+      "This project is a fully functional WhatsApp clone developed using React Native, designed to replicate the core features of the popular messaging app including real-time messaging, user authentication, and chat rooms.",
+    status: "Not Live",
+    link: "",
+    github: "https://github.com/ParthSharma5/Clone",
+    languagesUsed: ["React Native", "Firebase", "JavaScript"],
+    featured: true,
+    date: "2024",
+    duration: "3 months",
+    team: "Solo",
+  },
+
+  {
+    id: 7,
+    title: "Real-time Chat App",
+    description:
+      "A full-featured real-time chat application built using React Native, Supabase, and Clerk for authentication. This project focuses on creating a fast, secure, and scalable mobile chat experience with modern backend services.",
+    status: "Not Live",
+    link: "",
+    github: "https://github.com/ParthSharma5/RealChat",
+    languagesUsed: ["React Native", "TypeScript", "Supabase", "Clerk"],
+    featured: false,
+    date: "2026",
+    duration: "2 months",
     team: "Solo",
   },
 ];
